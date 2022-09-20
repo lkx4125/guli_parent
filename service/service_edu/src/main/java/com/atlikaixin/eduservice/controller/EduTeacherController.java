a076712c1360fad04d8f68088b7021f7bac6f315
likaixin <2601514316@qq.com>
2022-09-20 11:13:49 +0800

    1.0.2 (02-09-20AM)
    feature
      ·scope:service
      ·subject:add
               Unified return object
               Paging query and Conditional query
               Add and modify instructor

--- service/service_edu/src/main/java/com/atlikaixin/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atlikaixin/eduservice/controller/EduTeacherController.java
@@ -3,15 +3,19 @@ package com.atlikaixin.eduservice.controller;
 
 import com.atlikaixin.commonutils.R;
 import com.atlikaixin.eduservice.entity.EduTeacher;
+import com.atlikaixin.eduservice.entity.vo.TeacherQuery;
 import com.atlikaixin.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * <p>
@@ -31,7 +35,7 @@ public class EduTeacherController {
     private EduTeacherService teacherService;
     // 访问地址：http://localhost:8001/eduservice/teacher/findAll
 
-    // 查询讲师表所有数据
+    // 1 查询讲师表所有数据
     // rest风格
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
@@ -41,7 +45,7 @@ public class EduTeacherController {
     }
 
 
-    // 逻辑删除讲师的方法
+    // 2 逻辑删除讲师的方法
     @ApiOperation(value = "逻辑删除讲师")
     @DeleteMapping("{id}")
     public R removeTeacher(
@@ -54,6 +58,109 @@ public class EduTeacherController {
             return R.error();
         }
     }
+
+    // 3 分页查询讲师
+    // current 当前页
+    // limit 每页记录数
+    @ApiOperation(value = "分页查询讲师")
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(
+            @ApiParam(name = "current", value = "当前页", required = true)
+            @PathVariable long current,
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable long limit) {
+        // 创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        // 调用方法实现分页
+        // 调用方法时，底层封装，把分页所有数据封装到pageTeacher对象里面
+        teacherService.page(pageTeacher, null);
+        long total = pageTeacher.getTotal(); // 总记录数
+        List<EduTeacher> records = pageTeacher.getRecords(); // 数据list集合
+
+        //HashMap map = new HashMap();
+        //map.put("total", total);
+        //map.put("rows", records);
+        //return R.ok().data(map);
+
+        return R.ok().data("total", total).data("rows", records);
+
+    }
+
+    // 4 条件查询 分页
+    @ApiOperation(value = "分页条件查询讲师")
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@ApiParam(name = "current", value = "当前页", required = true)
+                                  @PathVariable long current,
+                                  @ApiParam(name = "limit", value = "每页记录数", required = true)
+                                  @PathVariable long limit,
+                                  @ApiParam(name = "TeacherQuery teacherQuery", value = "条件", required = true)
+                                  @RequestBody(required = false) TeacherQuery teacherQuery) {
+        // 创建对象
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        // 构建条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // mybatis 动态sql
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("begin", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("end", end);
+        }
+
+        // 调用方法实现条件查询分页
+        teacherService.page(pageTeacher, wrapper);
+
+        long total = pageTeacher.getTotal(); // 总记录数
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total", total).data("rows", records);
+
+    }
+
+    // 添加讲师接口
+    @ApiOperation(value = "新增讲师")
+    @PostMapping("addTeacher")
+    public R addTeacherI(
+            @ApiParam(name = "eduTeacher", value = "讲师对象", required = true)
+            @RequestBody EduTeacher eduTeacher) {
+        boolean save = teacherService.save(eduTeacher);
+        if (save) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    // 根据Id查寻讲师
+    @ApiOperation(value = "根据ID查询讲师")
+    @GetMapping("getTeacher/{id}")
+    public R getTeacher(@PathVariable String id) {
+        EduTeacher eduTeacher = teacherService.getById(id);
+        return R.ok().data("teacher", eduTeacher);
+    }
+
+    // 讲师修改功能
+    @ApiOperation(value = "根据ID修改讲师")
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean flag = teacherService.updateById(eduTeacher);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
 
 }
 
